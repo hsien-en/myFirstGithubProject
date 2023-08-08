b999441777f06320a2f5286a30a967a6fa3730c1
Shannon <hsienenwu@gmail.com>
2023-08-08 16:37:46 -0700

    project

--- public/index.js
+++ public/index.js
@@ -1,2 +1,21 @@
-const message = "awesome project"
-console.log(message)
+/**
+create color changinf button X Made Div
+recognixe a click X addEventListener ('click)
+where will click be recognixed X colorchanging
+get acces to color changing div X document.getElementById()
+write code for when click happens X changeColorPlease
+bg color changes
+ */
+let i = 0
+const bodyDiv = document.getElementById('bodyID')
+let colors = ['primary', 'danger', 'warning', 'information']
+
+function changeColorPlease(){
+bodyDiv.classList = `bg-${colors[i]}`
+i = ((i +1) % colors.length)
+}
+
+const colorChangingDiv = document.getElementById('colorChanging')
+colorChangingDiv.addEventListener('click', ()=>{changeColorPlease()})
+
+
